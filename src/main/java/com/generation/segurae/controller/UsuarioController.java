bd966275aa740560a93420b2d1ede3c7424fa9ea
João Vitor Diniz Alves <diniz.vitor.alves@gmail.com>
2026-08-03 18:49:39 -0300

    :sparkles: Controller Feita

---
+++ src/main/java/com/generation/segurae/controller/UsuarioController.java
@@ -0,0 +1,94 @@
+package com.generation.segurae.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.segurae.model.Usuario;
+import com.generation.segurae.repository.UsuarioRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+   // @Autowired
+   // private UsuarioService usuarioService;
+
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> getAll() {
+        return ResponseEntity.ok(usuarioRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        return usuarioRepository.findById(id)
+                // se encontrou, devolve 200 OK com a categoria no corpo
+                .map(ResponseEntity::ok)
+                // se não encontrou, devolve 404 Not Found (corpo vazio)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<Optional<Usuario>> getByTipo(@PathVariable String email) {
+        return ResponseEntity.ok(usuarioRepository.findByEmail(email));
+    }
+    
+    
+    /***
+     * 
+     * @PostMapping("/cadastrar")
+    public ResponseEntity<Usuario> post(@Valid @RequestBody Usuario usuario) {
+        return usuarioService.cadastrarUsuario(usuario)
+                .map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+    }
+     * ***/
+
+    @PutMapping
+    public ResponseEntity<Usuario> put(@Valid @RequestBody Usuario usuarios) {
+        return usuarioRepository.findById(usuarios.getId())
+                .map(resposta -> ResponseEntity.status(HttpStatus.OK)
+                        .body(usuarioRepository.save(usuarios)))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+   /***
+    * ---> Quando tiver a segurança
+    * 
+    *  @PostMapping("/logar")
+    public ResponseEntity<UsuarioLogin> autenticar(@Valid @RequestBody Optional<UsuarioLogin> usuarioLogin) {
+        return usuarioService.autenticarUsuario(usuarioLogin)
+                .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+    }
+    * 
+    * */
+
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        Optional<Usuario> tema = usuarioRepository.findById(id);
+        if (tema.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        usuarioRepository.deleteById(id);
+    }
+}
